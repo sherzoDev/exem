d46adca2c183025d247773dc0484a851f9aca95e
sherzoDev <mustafoqulovsherzodbek@gmail.com>
2022-10-20 17:14:32 +0500

    Add Pricing

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
 var elBtn = document.querySelector(".header__btn");
 var body = document.querySelector("body");
+var theme = document.querySelector(".theme");
+var darkMode = document.querySelector(".dark-mode");
 
 elBtn.addEventListener("click", function () {
   elBtn.parentElement.classList.toggle("show-bg");
   body.classList.toggle("scroll");
 })
+
+// theme.addEventListener("click" , function(){
+//     body.classList.toggle("dark-mode");
+// })
